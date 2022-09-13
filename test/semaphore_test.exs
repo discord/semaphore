b13655332e016630db42b65e42bf1a8ca5f60e72
oceans <sea@likeanocean.com>
2022-09-13 20:39:00 +0000

    ~

--- test/semaphore_test.exs
+++ test/semaphore_test.exs
@@ -42,12 +42,14 @@ defmodule SemaphoreTest do
 
   test "call_linksafe" do
     # Spawn a process that will exit due to a linked process exiting.
-    {pid, ref} = spawn_monitor(fn ->
-      Semaphore.call_linksafe(:foo, 5, fn ->
-        spawn_link(fn -> exit(:ded) end)
-        Process.sleep(:infinity)
+    {pid, ref} =
+      spawn_monitor(fn ->
+        Semaphore.call_linksafe(:foo, 5, fn ->
+          spawn_link(fn -> exit(:ded) end)
+          Process.sleep(:infinity)
+        end)
       end)
-    end)
+
     # Wait for the process to die.
     assert_receive {:DOWN, ^ref, :process, ^pid, :ded}
     # The leak should have occurred.
@@ -60,14 +62,59 @@ defmodule SemaphoreTest do
     assert Semaphore.count(:foo) == 0
   end
 
+  test "call_linksafe with full semaphore" do
+    # Assert similar behavior from call/3
+    assert Semaphore.call_linksafe(:foo, 5, fn -> :bar end) == :bar
+    assert Semaphore.count(:foo) == 0
+
+    # The call-safe table should be empty after a typical call to call_linksafe/3
+    assert :ets.lookup(:semaphore_call_safe, :foo) == []
+
+    assert Semaphore.acquire(:foo, 1) == true
+    assert Semaphore.call_linksafe(:foo, 1, fn -> :bar end) == {:error, :max}
+
+    # Reset for the following additional tests
+    Semaphore.reset(:foo)
+
+    # Spawn a couple processes that will exit due to a linked process exiting.
+    {pid1, ref1} =
+      spawn_monitor(fn ->
+        Semaphore.call_linksafe(:foo, 5, fn ->
+          spawn_link(fn -> exit(:ded) end)
+          Process.sleep(:infinity)
+        end)
+      end)
+
+    {pid2, ref2} =
+      spawn_monitor(fn ->
+        Semaphore.call_linksafe(:foo, 5, fn ->
+          spawn_link(fn -> exit(:ded) end)
+          Process.sleep(:infinity)
+        end)
+      end)
+
+    # Wait for the process to die.
+    assert_receive {:DOWN, ^ref1, :process, ^pid1, :ded}
+    assert_receive {:DOWN, ^ref2, :process, ^pid2, :ded}
+    # The leak should have occurred.
+    assert Semaphore.count(:foo) == 2
+    # Force a sweep.
+    Semaphore |> send(:timeout)
+    # This is just so that we can ensure the sweep was processed.
+    Semaphore |> :sys.get_state()
+    # The leak should now be fixed.
+    assert Semaphore.count(:foo) == 0
+  end
+
   test "acquire_linksafe" do
     # Spawn a process that will exit due to a linked process exiting.
-    {pid, ref} = spawn_monitor(fn ->
-      max = 5
-      true = Semaphore.acquire_linksafe(:name, :key, max)
-      true = Semaphore.acquire_linksafe(:name, :key2, max)
-      exit(:ded)
-    end)
+    {pid, ref} =
+      spawn_monitor(fn ->
+        max = 5
+        true = Semaphore.acquire_linksafe(:name, :key, max)
+        true = Semaphore.acquire_linksafe(:name, :key2, max)
+        exit(:ded)
+      end)
 
     # Wait for the process to die.
     assert_receive {:DOWN, ^ref, :process, ^pid, :ded}
@@ -86,11 +133,10 @@ defmodule SemaphoreTest do
     assert Semaphore.count(:name) == 1
 
     Semaphore.acquire_linksafe(:name, :key2, 5)
-    assert Semaphore.count(:name) == 1
+    assert Semaphore.count(:name) == 2
 
     Semaphore.release_linksafe(:name, :key)
     assert Semaphore.count(:name) == 1
-
 
     Semaphore.release_linksafe(:name, :key2)
     assert Semaphore.count(:name) == 0
